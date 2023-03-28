cf71b1b1dce0055591e30e835add19568174fe76
LuizHenriqueSilverio <luizhenriquesilverios@hotmail.com>
2023-03-28 10:42:35 -0300

    Atualizado - aula 28/03/2023

--- Projeto Agencia Bancária/3SI_comandos_28mar.sql
+++ Projeto Agencia Bancária/3SI_comandos_28mar.sql
@@ -2,3 +2,20 @@ DESC cliente;
 
 INSERT INTO cliente(idCliente, nome, cpf, dataNascimento)
 VALUES(NULL, "Afrânio Rosa", "777.777.777-77", "1958-06-12");
+
+SELECT * FROM cliente;
+
+/*
+	Criação de uma tabela virtual - VIEW para ordenar
+	os clientes em ordem alfabética crescente
+*/
+
+CREATE VIEW v_ClientesOrdemCrescente AS 
+(SELECT * FROM cliente ORDER BY nome);
+SHOW TABLES;
+
+/*
+	Listagem dos dados de uma view
+*/
+
+SELECT * FROM v_clientesordemcrescente;
