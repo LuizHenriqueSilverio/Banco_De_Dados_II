79ca3668893575805c0a0f184aed80af64061e88
LuizHenriqueSilverio <luizhenriquesilverios@hotmail.com>
2023-03-30 10:48:21 -0300

    Atualizado - aula 30/03/2023

--- Projeto Agencia Bancária/3SI_comandos_30mar.sql
+++ Projeto Agencia Bancária/3SI_comandos_30mar.sql
@@ -13,3 +13,16 @@ CREATE VIEW v_TotalFinanceiro AS
 (SELECT SUM(saldo) FROM conta);
 
 SELECT * FROM v_totalfinanceiro;
+
+/*
+	Comando para mostrar o nome do cliente, o tipo
+	de sua(s) conta(s) e respectivo(s) saldo(s).
+*/
+
+SELECT cli.nome, con.tipo, con.saldo FROM cliente AS cli
+INNER JOIN conta AS con
+INNER JOIN contavinculada AS cv
+ON cli.idCLIENTE = cv.CLIENTE_idCLIENTE
+AND con.idCONTA = cv.CONTA_idCONTA;
+
+SELECT * FROM v_contasclientes;
