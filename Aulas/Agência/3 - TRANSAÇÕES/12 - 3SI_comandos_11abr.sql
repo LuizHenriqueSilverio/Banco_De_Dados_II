73cbc74343fb86c0c54e8bd440f9db809e7614f2
LuizHenriqueSilverio <luizhenriquesilverios@hotmail.com>
2023-04-11 10:49:09 -0300

    Atualizado - aula 11/04/2023

--- Aulas/Agência/3 - TRANSAÇÕES/12 - 3SI_comandos_11abr.sql
+++ Aulas/Agência/3 - TRANSAÇÕES/12 - 3SI_comandos_11abr.sql
@@ -3,3 +3,13 @@
 	e depois a conta
 */
 
+SELECT * FROM conta;
+SELECT * FROM contavinculada;
+
+BEGIN;
+	DELETE FROM contavinculada WHERE CONTA_idCONTA = 1;
+	DELETE FROM conta WHERE idCONTA = 1;
+COMMIT;
+
+SELECT * FROM conta;
+SELECT * FROM contavinculada;
