48c9eb32d5999fc8b1dbb76e71bd8b7bca4b7c66
LuizHenriqueSilverio <luizhenriquesilverios@hotmail.com>
2023-05-25 09:56:54 -0300

    Atualizado - aula 25/05/2023

--- Aulas/setLc_Time_Names.sql
+++ Aulas/setLc_Time_Names.sql
@@ -1,5 +1,11 @@
-# Muda o padrão de formatação de datas para o formato brasileiro
+# Mostra configuração atual do padrão de formatação de datas (campo DATE)
 
 SELECT @@lc_time_names;
 
+# Muda o padrão de formatação de datas para o formato brasileiro
+
 SET lc_time_names = pt_br;
+
+# Configura para o formato brasileiro de forma global
+
+SET GLOBAL lc_time_names = pt_br;
